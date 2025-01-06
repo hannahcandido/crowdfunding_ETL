3818d4ab86f1a39907ba6525db0199be34ad1491
hannahcandido <eneann08@yahoo.com>
2025-01-06 18:29:44 -0500

    queries.sql

--- queries.sql
+++ queries.sql
@@ -1,11 +1,20 @@
+--Display campaign table
 SELECT *
 FROM campaign
 
+--Display category table
 SELECT *
 FROM category
 
+--Display subcategory table
 SELECT *
 FROM subcategory
 
+--Display contacts table
 SELECT *
 FROM contacts
+
+--Display contact id, First and last name as well as company name
+SELECT c."contact_id", c."First_name", c."Last_name", ca."company_name"
+FROM "contacts" c
+JOIN "campaign" ca ON c."contact_id" =ca."contact_id";
